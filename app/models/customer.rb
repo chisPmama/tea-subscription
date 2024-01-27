4f3712c5871c1767c6504b25678a073ca10339a4
Anthea Yur <acyur6@gmail.com>
2024-01-26 17:28:40 -0700

    Add testing for Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,6 @@ class Customer < ApplicationRecord
   has_one :subscription
   has_many :customer_teas
   has_many :teas, through: :customer_teas
-  has_many :teas, through: :subscriptions
 
   validates :first_name, presence: true
   validates :last_name, presence: true
---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  describe "relationships" do
+    it { should have_one :subscription }
+    it { should have_many :customer_teas }
+    it { should have_many(:teas).through(:customer_teas) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:address) }
+  end
+end
